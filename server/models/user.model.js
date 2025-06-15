a3e613e1a2054063393eb74383b2bb9b414f2a08
Prit patel <148608375+pritpatel2412@users.noreply.github.com>
2025-06-15 15:14:03 +0530

    Update user.model.js

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -16,7 +16,7 @@ const userSchema = new mongoose.Schema({
     role:{
         type:String,
         enum:["instructor", "student"],
-        default:'student'
+        default:'instructor'
     },
     enrolledCourses:[
         {
